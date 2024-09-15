4c91a15bc89c345eb8273506af71a33de1ea1da5
mejrakucevic <mejrakucevic420@gmail.com>
2024-09-15 12:51:05 +0200

    .

--- GameShelf/GS/views.py
+++ GameShelf/GS/views.py
@@ -11,6 +11,10 @@ def catalog(request):
     games = Game.objects.all()  # Fetch all games from the database
     return render(request, 'GS/catalog.html', {'games': games})
 
+from django.shortcuts import render, get_object_or_404
+from .models import Game
+from .utils import fetch_game_details
+
 def game_detail(request, game_id):
     # Fetch the game from your local database using the game_id
     game = get_object_or_404(Game, id=game_id)
@@ -20,18 +24,19 @@ def game_detail(request, game_id):
 
     if game_data:
         context = {
-            'title': game_data.get('name', 'Unknown'),
+            'title': game_data.get('title', 'Unknown'),
             'image_link': game_data.get('background_image', ''),
             'description': game_data.get('description', 'No description available'),
             'rating': game_data.get('rating', 'N/A'),
-            'release_date': game_data.get('released', 'Unknown'),
-            'publisher': ', '.join([publisher['name'] for publisher in game_data.get('publishers', [])]) or 'Unknown',
-            'developer': ', '.join([developer['name'] for developer in game_data.get('developers', [])]) or 'Unknown',
+            'release_date': game_data.get('release_date', 'Unknown'),
+            'publisher': game_data.get('publisher', 'Unknown'),
+            'developer': game_data.get('developer', 'Unknown'),
         }
     else:
         context = {'error': 'Game details not found'}
 
     return render(request, 'GS/game_detail.html', context)
+
 
 
 
